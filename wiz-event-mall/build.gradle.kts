81bbc93d5fb1bd9bb23591fa29aa152ea6da4412
slolee <slolee@naver.com>
2024-02-18 22:20:14 +0900

    Feature: 동시성 문제 발생하는 테스트 코드 작성

--- wiz-event-mall/build.gradle.kts
+++ wiz-event-mall/build.gradle.kts
@@ -36,6 +36,10 @@ dependencies {
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.mockk:mockk:1.13.9")
+	testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+	testImplementation("io.kotest:kotest-assertions-core:5.7.2")
+	testImplementation("com.h2database:h2")
 }
 
 tasks.withType<KotlinCompile> {
