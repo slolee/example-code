925597ecfb2106f9eeea497e047cd963f8dcc7a0
slolee <slolee@naver.com>
2024-06-07 20:41:43 +0900

    [wiz-todo] mockk & kotest 의존성 추가

--- wiz-todo/build.gradle.kts
+++ wiz-todo/build.gradle.kts
@@ -26,9 +26,13 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.h2database:h2")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("io.mockk:mockk:1.13.9")
+    testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.7.2")
 }
 
 kotlin {
