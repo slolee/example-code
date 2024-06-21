a11186664923a22d522f6e90480d1b0c0d72834a
slolee <slolee@naver.com>
2024-06-22 00:01:34 +0900

    [wiz-cross-check] Init project

---
+++ wiz-cross-check/src/main/kotlin/com/example/wizcrosscheck/WizCrossCheckApplication.kt
@@ -0,0 +1,11 @@
+package com.example.wizcrosscheck
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WizCrossCheckApplication
+
+fun main(args: Array<String>) {
+    runApplication<WizCrossCheckApplication>(*args)
+}
---
+++ wiz-cross-check/src/test/kotlin/com/example/wizcrosscheck/WizCrossCheckApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.wizcrosscheck
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WizCrossCheckApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
